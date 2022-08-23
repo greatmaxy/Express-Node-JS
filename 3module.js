28e34f78bbd288e8250ab47424113719e6323326
greatmaxy <81714055+greatmaxy@users.noreply.github.com>
2022-08-23 23:56:38 +0530

    Create 3module.js

---
+++ 3module.js
@@ -0,0 +1,13 @@
+const john = 'john';
+const peter = 'peter';
+
+const sayHi = (name) => {
+    console.log(`Hello there ${name}`);
+}
+
+sayHi('susan')
+sayHi(john)
+sayHi(peter)
+
+//Modules - Encapsulated codes
+//Every file is a module
