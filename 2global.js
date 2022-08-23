f47e391c5f86b1210de8649e283beeda90826190
greatmaxy <81714055+greatmaxy@users.noreply.github.com>
2022-08-23 23:46:36 +0530

    Create 2global.js

---
+++ 2global.js
@@ -0,0 +1,11 @@
+//require - function to use module
+//module - info about current module
+//process - info about enviornment it is being executed (it is very important)
+
+//__dirname
+
+//up arrow to see previous things in terminals
+
+setInterval(()=>{
+    console.log("hello world");
+},1000);
