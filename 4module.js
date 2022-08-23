1d883f6d55073dc0f3a4c3416064084825002d82
greatmaxy <81714055+greatmaxy@users.noreply.github.com>
2022-08-24 00:01:44 +0530

    Create 4module.js

---
+++ 4module.js
@@ -0,0 +1,12 @@
+//every file is a module
+
+const secret = "SUPER SECRET"
+
+
+const john = 'john';
+const peter = 'peter';
+
+module.exports = {john, peter};
+
+const names = require('./t2.js')
+console.log(names);
